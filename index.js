6146ed712d34bd7ae6b7b2df317694bb73993368
salman shaik <dukesalman5@gmail.com>
2017-12-27 13:03:52 +0530

    added help module to the game

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const loadGame=function(){
   window.location.href='./src/main.html';
 };
-
+const loadHelp=function() {
+  window.location.href='./src/help.html';
+}
 const addClickListenerToButton=function() {
   let button=document.getElementById('button');
   button.onclick=loadGame;
+  let help=document.getElementById('help');
+  help.onclick=loadHelp;
 };
 
 const begin=function() {
